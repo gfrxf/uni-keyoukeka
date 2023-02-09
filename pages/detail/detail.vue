610b0552438e44ad7c1ba7261ba9d84a5394ed6e
gfrxf <2817720040@qq.com>
2023-02-09 22:00:28 +0800

    详情

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -2,93 +2,166 @@
 	<!-- <view ref="contentEl" class="content">
 		111
 	</view> -->
-	<rich-text class="content" :nodes="content.value">
-
-	</rich-text>
+	<!-- 	<rich-text ref="conEl" class="content">
+		111111
+	</rich-text> -->
 	<!-- <rich-text>
 
 	</rich-text> -->
+	<!-- <div ref="conEl" class="content"></div> -->
+	<!-- 	<view class="" v-html="content">
+
+	</view> -->
+	<!-- <rich-text :nodes="content"></rich-text> -->
+	<mp-html :content="contentEl"></mp-html>
+
 
 </template>
 
-<script setup>
-	import {
-		onLoad
-	} from '@dcloudio/uni-app'
-	import {
-		nextTick,
-		onMounted,
-		ref,
-		getCurrentInstance,
-		toRef
-	} from 'vue'
+// <script setup>
+	// 	import {
+	// 		onLoad
+	// 	} from '@dcloudio/uni-app'
+	// 	import {
+	// 		nextTick,
+	// 		onMounted,
+	// 		ref,
+	// 		getCurrentInstance,
+	// 		toRef
+	// 	} from 'vue'
+	// 	import {
+	// 		getDetails
+	// 	} from '@/service/home.js'
+	// 	// 获取到页面传递过来的URL参数
+	// 	const props = defineProps({
+	// 		tagId: {
+	// 			type: String,
+	// 			default: ''
+	// 		}
+	// 	})
+	// 	// console.log('iid=', props.iid);
+
+	// 	// 获取this
+	// 	const {
+	// 		ctx
+	// 	} = getCurrentInstance()
+	// 	const _this = ctx
+	// 	const contentEl = ref()
+	// 	let content = ref('')
+	// 	let test = '<div style="text-align:center;"><img src="https://web-assets.dcloud.net.cn/unidoc/zh/uni@2x.png"/></div>'
+	// 	// const tagId = ref()
+
+	// 	// onLoad((options) => {
+	// 	// 	// console.log('options=>', options);
+	// 	// 	tagId.value = options.tagId
+	// 	// 	console.log(tagId, 'tagid');
+	// 	// })
+	// 	onLoad(() => {
+	// 		getdetail(props.tagId)
+	// 		// console.log(content, 'content');
+
+	// 	})
+	// 	onMounted(() => {
+	// 		// console.log(contentEl.value, 'contentEl');
+	// 	})
+	// 	async function getdetail(id) {
+	// 		const res = await getDetails(id)
+
+
+
+	// 		content.value = htmlUnescape(res.data.content)
+	// 		console.log(content.value, 'con');
+	// 		console.log(typeof content.value, 'type');
+
+
+	// 	}
+
+	// 	function htmlUnescape(html) {
+	// 		return html.replace(/&lt;|&gt;|&quot;|&amp;/g, match => {
+	// 			switch (match) {
+	// 				case '&lt;':
+	// 					return '<';
+	// 				case '&gt;':
+	// 					return '>';
+	// 				case '&quot;':
+	// 					return '"';
+	// 				case '&amp;':
+	// 					return '&';
+
+
+
+	// 			}
+	// 		})
+	// 	}
+
+	// 	function changeContent() {
+
+	// 	}
+	// 
+</script>
+
+<script>
 	import {
 		getDetails
 	} from '@/service/home.js'
-	// 获取到页面传递过来的URL参数
-	const props = defineProps({
-		tagId: {
-			type: String,
-			default: ''
-		}
-	})
-	// console.log('iid=', props.iid);
-
-	// 获取this
-	const {
-		ctx
-	} = getCurrentInstance()
-	const _this = ctx
-	const contentEl = ref()
-	let content = ref('')
-	let test = '<div style="text-align:center;"><img src="https://web-assets.dcloud.net.cn/unidoc/zh/uni@2x.png"/></div>'
-	// const tagId = ref()
-
-	// onLoad((options) => {
-	// 	// console.log('options=>', options);
-	// 	tagId.value = options.tagId
-	// 	console.log(tagId, 'tagid');
-	// })
-	onLoad(() => {
-		getdetail(props.tagId)
-		// console.log(content, 'content');
-
-	})
-	onMounted(() => {
-		// console.log(contentEl.value, 'contentEl');
-	})
-	async function getdetail(id) {
-		const res = await getDetails(id)
-
-
-
-		content.value = htmlUnescape(res.data.content)
-		console.log(content.value, 'con');
-		console.log(typeof content.value, 'type');
-
-
-	}
-
-	function htmlUnescape(html) {
-		return html.replace(/&lt;|&gt;|&quot;|&amp;/g, match => {
-			switch (match) {
-				case '&lt;':
-					return '<';
-				case '&gt;':
-					return '>';
-				case '&quot;':
-					return '"';
-				case '&amp;':
-					return '&';
-
-
-
+	export default {
+		data() {
+			return {
+				tagId: 1,
+				contentEl: '',
+				test: '<div>Hello World!</div>'
 			}
-		})
-	}
+		},
+		// props: {
+		// 	tagId: {
+		// 		type: String,
+		// 		default: '',
 
-	function changeContent() {
+		// 	}
+		// },
+		onLoad(option) {
 
+			this.tagId = option.tagId
+			console.log(this.tagId, 'tagid');
+			this.getdetail(this.tagId)
+
+		},
+		mounted() {
+			// console.log(111);
+			// console.log(this.$refs.conEl, 'el');
+			// const conEL = document.querySelector('.content')
+			// console.log(conEL, 'conel');
+		},
+		methods: {
+			async getdetail(id) {
+				const res = await getDetails(id)
+
+				this.contentEl = this.htmlUnescape(res.data.content)
+				// this.$forceUpdate()
+
+				// console.log(this.content, 'content');
+				// console.log(typeof this.content, 'type ');
+
+			},
+			htmlUnescape(html) {
+				return html.replace(/&lt;|&gt;|&quot;|&amp;/g, match => {
+					switch (match) {
+						case '&lt;':
+							return '<';
+						case '&gt;':
+							return '>';
+						case '&quot;':
+							return '"';
+						case '&amp;':
+							return '&';
+
+
+
+					}
+				})
+			}
+
+		}
 	}
 </script>
 
--- unpackage/dist/dev/mp-weixin/pages/detail/detail.js
+++ unpackage/dist/dev/mp-weixin/pages/detail/detail.js
@@ -2,30 +2,35 @@
 const common_vendor = require("../../common/vendor.js");
 const service_home = require("../../service/home.js");
 require("../../service/index.js");
-const _sfc_main = {
-  __name: "detail",
-  props: {
-    tagId: {
-      type: String,
-      default: ""
-    }
+if (!Array) {
+  const _easycom_mp_html2 = common_vendor.resolveComponent("mp-html");
+  _easycom_mp_html2();
+}
+const _easycom_mp_html = () => "../../uni_modules/mp-html/components/mp-html/mp-html.js";
+if (!Math) {
+  _easycom_mp_html();
+}
+const __default__ = {
+  data() {
+    return {
+      tagId: 1,
+      contentEl: "",
+      test: "<div>Hello World!</div>"
+    };
   },
-  setup(__props) {
-    const props = __props;
-    common_vendor.ref();
-    let content = common_vendor.ref("");
-    common_vendor.onLoad(() => {
-      getdetail(props.tagId);
-    });
-    common_vendor.onMounted(() => {
-    });
-    async function getdetail(id) {
+  onLoad(option) {
+    this.tagId = option.tagId;
+    console.log(this.tagId, "tagid");
+    this.getdetail(this.tagId);
+  },
+  mounted() {
+  },
+  methods: {
+    async getdetail(id) {
       const res = await service_home.getDetails(id);
-      content.value = htmlUnescape(res.data.content);
-      console.log(content.value, "con");
-      console.log(typeof content.value, "type");
-    }
-    function htmlUnescape(html) {
+      this.contentEl = this.htmlUnescape(res.data.content);
+    },
+    htmlUnescape(html) {
       return html.replace(/&lt;|&gt;|&quot;|&amp;/g, (match) => {
         switch (match) {
           case "&lt;":
@@ -39,12 +44,19 @@ const _sfc_main = {
         }
       });
     }
+  }
+};
+const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
+  __name: "detail",
+  setup(__props) {
     return (_ctx, _cache) => {
       return {
-        a: common_vendor.unref(content).value
+        a: common_vendor.p({
+          content: _ctx.contentEl
+        })
       };
     };
   }
-};
+});
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "/Users/xiongfeng/Desktop/\u53EF\u6709\u79D1\u5361/keyoukeka/pages/detail/detail.vue"]]);
 wx.createPage(MiniProgramPage);
